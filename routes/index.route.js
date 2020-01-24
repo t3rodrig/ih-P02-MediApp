1c68808e30109c3d537d6028e9644d826369d6bb
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-01-23 22:01:06 -0600

    citas

--- routes/index.route.js
+++ routes/index.route.js
@@ -5,4 +5,14 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.get("/reserve", (req, res, next) => {
+  // obtener las horas disponibles por semana
+  const days = {
+    monday: ["11:00", "12:00"],
+    tuesday: ["11:00", "12:00"],
+    wednesday: ["11:00", "13:00"]
+  };
+  res.render('reserve', {days});
+});
+
 module.exports = router;
