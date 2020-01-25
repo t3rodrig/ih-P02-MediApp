204b17ea07f756eeb7166457f4fa6a5bf1c681b6
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-01-25 14:00:18 -0600

    Agenda

--- routes/index.route.js
+++ routes/index.route.js
@@ -7,10 +7,17 @@ router.get("/", (req, res, next) => {
 
 router.get("/reserve", (req, res, next) => {
   // obtener las horas disponibles por semana
+  /*
+  const data = [
+    {Fecha: [{citaHora, citaID}, {}, {}, ...]},
+    {},
+    {},
+    ...];
+  */
   const days = {
-    monday: ["11:00", "12:00"],
-    tuesday: ["11:00", "12:00"],
-    wednesday: ["11:00", "13:00"]
+    'Lunes': ["10:00", "11:00"],
+    'Martes': ["12:00", "13:00"],
+    'Miércoles': ["15:00", "16:00"]
   };
   res.render('reserve', {days});
 });
