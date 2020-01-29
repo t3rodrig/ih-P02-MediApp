3e9bf2a7bb8c0e714bc860a950f91ed183cfd31b
Edgar Zea <cesszea@icloud.com>
2020-01-28 19:58:38 -0600

    Sign Up Patient Validation :tada:

--- routes/signup.route.js
+++ routes/signup.route.js
@@ -43,58 +43,20 @@ router.post("/doctor", async (req, res, next) => {
       const salt = await bcrypt.genSalt(10);
       const hashPass = await bcrypt.hash(password, salt);
 
-      await Doctor.create({ name, password: hashPass, email, role });
-    }
-  } catch {}
-});
-
-router.post("/doctor", async (req, res, next) => {
-  const name = req.body.inputName;
-  const idCard = req.body.inputIDcard;
-  const email = req.body.inputEmail;
-  const password = req.body.inputPassword;
-  const confirmPassword = req.body.inputConfirmPassword;
-
-  try {
-    if (name === "" || password === "" || email === "") {
-      return res.render("signup", {
-        message: "Todos los campos son requeridos."
-      });
-    }
-
-    const emailExists = await Doctor.findOne({ email });
-
-    if (emailExists) {
-      return res.render("index", {
-        message: "Este usuario ya existe."
-      });
-    }
-
-    if (idCard === "") {
-      return res.render("index", {
-        message:
-          "Es requerido validar tu profesión para registrarte como Médico"
-      });
-    } else {
-    }
-
-    if (password === confirmPassword) {
-      const salt = await bcrypt.genSalt(10);
-      const hashPass = await bcrypt.hash(password, salt);
-
-      await Doctor.create({ name, password: hashPass, email, role });
+      await Doctor.create({ name, password: hashPass, email });
     }
   } catch {}
 });
 
 router.post("/patient", async (req, res, next) => {
   const name = req.body.inputName;
+  const paternalLastName = req.body.inputPaternalLastName;
   const email = req.body.inputEmail;
   const password = req.body.inputPassword;
   const confirmPassword = req.body.inputConfirmPassword;
 
   try {
-    if (name === "" || password === "" || email === "") {
+    if (name === "" || paternalLastName || password === "" || email === "") {
       return res.render("signup", {
         message: "Todos los campos son requeridos."
       });
@@ -103,20 +65,29 @@ router.post("/patient", async (req, res, next) => {
     const emailExists = await Patient.findOne({ email });
 
     if (emailExists) {
-      return res.render("index", {
+      return res.render("signup", {
         message: "Este usuario ya existe."
       });
     }
 
-    if (password === confirmPassword) {
-      const salt = await bcrypt.genSalt(10);
-      const hashPass = await bcrypt.hash(password, salt);
-
-      await Patient.create({ name, password: hashPass, email, role });
-
-      const user = await Patient.findOne({ email });
+    if (password !== confirmPassword) {
+      return res.render("signup", { message: "Las contraseñas no coinciden" });
     }
-  } catch {}
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(password, salt);
+
+    await Patient.create({ name, paternalLastName, password: hashPass, email });
+
+    const user = await Patient.findOne({ email });
+
+    req.session.currretUser = user;
+
+    console.log(user);
+    res.render("profile", { user });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
