3c2f5e6152c47cdb984497619925d3c6aa2b451f
Edgar Zea <cesszea@icloud.com>
2020-01-30 21:55:22 -0600

    Cloudinary package and editing profile route

--- models/Patient.js
+++ models/Patient.js
@@ -1,21 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const patientSchema = new Schema({
-    name: {type: String, required: true},
-    paternalLastName: {type: String, required: true},
-    maternalLastName: {type: String},
-    email: {type: String, match: /[\w\.-]+@[\w\.-]+\.[\w\.]+/, required: true},
-    password: {type: String, required: false},
-    appointment: [{type: Schema.Types.ObjectId, ref: 'Appointment'}]
-
-}, {
-    timestamps: {
-        createdAt: 'created_at',
-        updatedAt: 'updated_at'
+const patientSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    paternalLastName: { type: String, required: true },
+    maternalLastName: { type: String },
+    email: {
+      type: String,
+      match: /[\w\.-]+@[\w\.-]+\.[\w\.]+/,
+      required: true
+    },
+    password: { type: String, required: true },
+    appointment: [{ type: Schema.Types.ObjectId, ref: "Appointment" }],
+    profilePic: {
+      type: String
     }
-});
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
 
-const Patient = mongoose.model('Patient', patientSchema);
+const Patient = mongoose.model("Patient", patientSchema);
 
 module.exports = Patient;
--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const uploadCloud = require("../config/cloudinary");
 const Doctor = require("../models/Doctor");
 const Patient = require("../models/Patient");
 
 router.get("/", (req, res, next) => {
-  res.render("profile");
+  const user = req.session.currentUser;
+
+  if (!user) {
+    res.redirect("/login");
+  }
+
+  res.render("profile", user);
 });
 
 router.get("/edit", (req, res, next) => {
