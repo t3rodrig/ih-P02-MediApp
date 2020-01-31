54c2e5d9c0edee692f19a6e9d1c3b166a0aa3e80
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-01-31 11:02:19 -0600

    Update login route

--- routes/login.route.js
+++ routes/login.route.js
@@ -7,22 +7,57 @@ router.get("/", (req, res, next) => {
   res.render("login");
 });
 
-router.post("/", async (req, res, next) => {
+router.post("/doctor", async (req, res, next) => {
   try {
-    res.redirect("profile");
-    // const { email, password } = req.body;
-    // console.log(req.body);
 
-    // if (!email || !password) {
-    //   return res.render("login", {
-    //     message: "Todos los campos son obligatorios."
-    //   });
-    // }
+    const email = req.body.inputEmail;
+    const password = req.body.inputPassword;
+
+    if (!email || !password) {
+      return res.render("login", {
+        messageDoc: "Todos los campos son obligatorios."
+      });
+    }
 
     // const doctor = await Doctor.findOne({ email });
-    // const patient = await Patient.findOne({ email });
     // console.log(doctor);
-    // console.log(patient);
+
+    // if (!doctor || !patient) {
+    //   return res.render("login", {
+    //     message: "El usuario no existe."
+    //   });
+    // } else {
+    //   if (
+    //     bcrypt.compareSync(password, patient.password) ||
+    //     bcrypt.compareSync(password, doctor.password)
+    //   ) {
+    //     req.session.currentUser = user;
+    //     res.redirect("profile");
+    //   } else {
+    //     res.render("login", {
+    //       message: "Los campos no coinciden."
+    //     });
+    //   }
+    // }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/patient", async (req, res, next) => {
+  try {
+    // res.redirect("/profile");
+    const email = req.body.inputEmail;
+    const password = req.body.inputPassword;
+
+    if (!email || !password) {
+      return res.render("login", {
+        messagePat: "Todos los campos son obligatorios."
+      });
+    }
+
+    const patient = await Patient.findOne({ email });
+    console.log(patient);
 
     // if (!doctor || !patient) {
     //   return res.render("login", {
