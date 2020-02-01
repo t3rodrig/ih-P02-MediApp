254a89c24a173bc6141cc8815219906a70d72670
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-02-01 11:01:56 -0600

    Update route and view for profile patient

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -26,17 +26,8 @@ router.get("/doctor/:personId", async (req, res, next) => {
 });
 
 router.get("/patient/edit", (req, res, next) => {
-  let personId = req.query.personId;
-  if (!/^[0-9a-fA-F]{24}$/.test(personId)) {
-    return res.status(404).render("not-found");
-  }
-
   const user = req.session.currentUser;
-  if (personId !== user._id) {
-    return res.status(404).render("not-found");
-  }
-
-  res.render("editProfile", { user });
+  res.render("editProfilePatient", { user });
 });
 
 router.get("/patient/:personId", async (req, res, next) => {
@@ -49,7 +40,7 @@ router.get("/patient/:personId", async (req, res, next) => {
   if (!user) {
     return res.status(404).render("not-found");
   }
-  res.render("profile", { user });
+  res.render("profile-patient", { user });
 });
 
 module.exports = router;
