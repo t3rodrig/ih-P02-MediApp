1d1d802a41f9ba997fb3295de904bae00969abff
Tonalli <tonalli@xanum.uam.mx>
2020-01-31 18:47:01 -0600

    Create individual profile

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,21 +1,57 @@
 const express = require("express");
 const router = express.Router();
-const uploadCloud = require("../config/cloudinary");
+// const uploadCloud = require("../config/cloudinary");
 const Doctor = require("../models/Doctor");
 const Patient = require("../models/Patient");
 
-router.get("/", (req, res, next) => {
-  const user = req.session.currentUser;
-
-  if (!user) {
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
     res.redirect("/login");
   }
-
-  res.render("profile", user);
 });
 
-router.get("/edit", (req, res, next) => {
-  res.render("editProfile");
+
+router.get("/doctor/:personId", async (req, res, next) => {
+  let personId = req.params.personId;
+  if (!/^[0-9a-fA-F]{24}$/.test(personId)) { 
+    return res.status(404).render('not-found');
+  }
+
+  const user = await Doctor.findById(personId);
+  if (!user) {
+    return res.status(404).render('not-found');
+  }
+  res.render("profile", { user });
+});
+
+
+router.get("/patient/edit", (req, res, next) => {
+  let personId = req.query.personId;
+  if (!/^[0-9a-fA-F]{24}$/.test(personId)) { 
+    return res.status(404).render('not-found');
+  }
+
+  const user = req.session.currentUser;
+  if (personId !== user._id){
+    return res.status(404).render('not-found');
+  }
+
+  res.render("editProfile", { user });
+});
+
+router.get("/patient/:personId", async (req, res, next) => {
+  let personId = req.params.personId;
+  if (!/^[0-9a-fA-F]{24}$/.test(personId)) { 
+    return res.status(404).render('not-found');
+  }
+
+  const user = await Patient.findById(personId);
+  if (!user) {
+    return res.status(404).render('not-found');
+  }
+  res.render("profile", { user });
 });
 
 module.exports = router;
