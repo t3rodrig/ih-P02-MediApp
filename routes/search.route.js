5441c06d86ab23b9b5a05ed2945e48a8ec616714
Tonalli Rodriguez <tonalli@xanum.uam.mx>
2020-02-06 20:54:51 -0600

    update search route

--- routes/search.route.js
+++ routes/search.route.js
@@ -24,20 +24,34 @@ router.get("/", async (req, res, next) => {
 
 router.get("/advanced", async (req, res, next) => {
   const user = req.session.currentUser;
-  const data = req.query;
-  console.log(req.query);
+  const isEmpty = Object.entries(req.query).length === 0;
 
-  try {
-    await Doctor.find(data).then(doctors => {
-      if (user) {
-        res.render("search-advanced", { user, doctors });
-      } else {
-        res.render("search-advanced", doctors);
-      }
-    });
-  } catch (error) {
-    console.log(error);
-    res.render("not-found");
+  if (isEmpty) {
+    if (user) {
+      res.render("search-advanced", { user });
+    } else {
+      res.render("search-advanced");
+    }
+  } else {
+    try {
+      const data = {
+        "address.borough": req.query.borough,
+        "specialty": req.query.specialty
+      };
+
+      await Doctor.find(data).then(doctors => {
+      // await Doctor.find(data).then(doctors => {
+        console.log({doctors});
+        if (user) {
+          res.render("search-advanced", { user, doctors });
+        } else {
+          res.render("search-advanced", doctors);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      res.render("not-found");
+    }
   }
 });
 
